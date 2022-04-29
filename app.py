39e4f46bf63df30481d512b47bcbb21d06896d17
Christoforos Vradis <chrisvradis00@gmail.com>
2022-04-30 00:50:46 +0300

    feat(deal): deal cards functionality

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-import random
+'''
+    The War Card Game!
+'''
+from ast import arguments
+from random import shuffle
 
 SUITS = ['C', 'S', 'H', 'D']
 RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
@@ -17,6 +21,13 @@ class Card:
         return self.rank + ' of '+self.suit + ' with value ' + str(self.value)
 
 
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.card_list = []
+
+
 class Deck:
 
     def __init__(self):
@@ -24,14 +35,52 @@ class Deck:
         self.build()
 
     def build(self):
+        '''
+            Creates the deck for the players to play.
+        '''
         for suit in SUITS:
             for rank in RANKS:
                 card = Card(suit, rank)
                 self.cards.append(card)
-        random.shuffle(self.cards)
+
+        print(f'Deck was created with {len(self.cards)} cards.\n')
+
+    def shuffle(self):
+        shuffle(self.cards)
+
+    def deal(self, *args):
+        self.hands = [self.cards[i::len(args)]
+                      for i in range(0, len(args))]
+        try:
+            args[0].cards = self.hands[0]
+            args[1].cards = self.hands[1]
+        except:
+            print('Not enough players')
 
 
+def introduction():
+    '''
+        Prints the rules of the game.
+    '''
+    print('\nWelcome to War! The player who is out of card loses !')
+    print('\t-Both players start with a deck of 26 cards')
+    print("\t-Both players draw a card. If your card's value is bigger than your opponent you put the card in your deck")
+    print('\t-If the value is the same you draw another and the winner gets all the cards')
+    print('\t-The player with 0 cards left loses!')
+    print('\n---------\n')
+
+
+def start_game(deck, p1, p2):
+    print(f'Starting the game! {p1.name} vs {p2.name}! Fight!')
+
+
+introduction()
 my_deck = Deck()
-for card in my_deck.cards:
-    print(card)
-    print(f'\t{card.value}')
+my_deck.shuffle()
+
+p1 = Player('Nick')
+p2 = Player('Bob')
+
+my_deck.deal(p1, p2)
+
+start_game(my_deck, p1, p2)
